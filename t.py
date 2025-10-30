4eacb02742ad68c1d9c3bf70c0d700d5d342b7a5
Rahul <kumarrahul@seasiainfotech.com>
2025-10-30 17:16:56 +0530

    Initial commit

---
+++ t.py
@@ -0,0 +1,8 @@
+import os
+username = os.getenv("username")
+password = os.getenv("password")
+remote_host = os.getenv("remote_host")
+port = os.getenv("port")
+database_name = os.getenv("database_name")
+
+print(f"postgresql://{username}:{password}@{remote_host}:{port}/{database_name}")
