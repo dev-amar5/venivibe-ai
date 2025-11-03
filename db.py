514250dea56caa0f81bb1356af351af861a37f7b
Rahul <kumarrahul@seasiainfotech.com>
2025-11-03 17:31:51 +0530

    CHANGED ENV VARAIBLES

--- db.py
+++ db.py
@@ -7,17 +7,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-username = os.getenv("username")
-password = os.getenv("password")
-remote_host = os.getenv("remote_host")
-port = os.getenv("port")
-database_name = os.getenv("database_name")
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+remote_host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+database_name = os.getenv("DB_NAME")
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # root = "/home/rahul/PycharmProjects/venivibe"
 root = os.path.dirname(os.path.abspath(__file__))
 
-db_path = os.path.join(root, "zali.db")  # full path to DB file
+# db_path = os.path.join(root, "zali.db")  # full path to DB file
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://rahul:123@localhost:5432/db1"
 SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{remote_host}:{port}/{database_name}"
 
